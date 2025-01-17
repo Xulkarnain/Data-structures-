de3cbd51f331a4a97930397399e386039b883f7f
Zulkarnain <darzulkarnain@gmail.com>
2025-01-17 21:55:38 +0530

    Add linear and binary search functions to Array ADT

--- Array_ADT/Insert.c
+++ Array_ADT/Insert.c
@@ -56,6 +56,43 @@ void delete(struct Array *arr ,int index)
 	}
 }
 
+int Lsearch(struct array *arr , int key)
+{
+    int i;
+    for(i=0 ; i<arr->length ; i++)
+    {
+        if(key==arr->A[i])
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int Bsearch(struct array *arr , int key)
+{
+    int l,h,mid;
+    l=0;
+    h=arr->length-1;
+    while(l<=h)
+    {
+        mid=(l+h)/2;
+        if(key==arr->A[mid])
+        {
+            return mid;
+        }
+        else if(key<arr->A[mid])
+        {
+            h=mid-1;
+        }
+        else
+        {
+            l=mid+1;
+        }
+    }
+    return -1;
+}
+
 int main()
 {
     struct Array arr = {{2,3,4,5,6}, 10, 5};
